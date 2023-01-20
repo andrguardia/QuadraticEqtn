d86fe4bccf0a1ed4b4af3d5ef55f31b63eb56790
Andre Guardia <andre.guardiaq@gmail.com>
2023-01-20 17:40:43 -0600

    Initial Commit

---
+++ QuadraticEqtn/QuadraticEqtnApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadraticEqtnApp.swift
+//  QuadraticEqtn
+//
+//  Created by IIT Phys 440 on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct QuadraticEqtnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
